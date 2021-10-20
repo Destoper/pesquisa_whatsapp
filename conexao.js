8058ad94586e56d366c08f96d9f8757b58a5e0ab
Márcio dos Santos <marciojunior.gm@gmail.com>
2021-10-19 21:44:03 -0300

    comentários das alterações

--- conexao.js
+++ conexao.js
@@ -1,7 +1,7 @@
 const Banco_Dados = require('mysql');
 
 module.exports = Banco_Dados.createConnection({
-    host: "192.168.0.102",
+    host: "192.168.0.100",
     user: "root",
     password: "root",
     database: "pesquisa"
